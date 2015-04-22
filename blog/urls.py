43e80a0f1cc38241648d1bd13c06083abf5ba06f
Nicole <nicole.rego@live.com>
2015-04-22 17:23:19 -0400

    NA-006 Editing urls for new models and links to individual posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.article_list),
+	url(r'^blog/(?P<CodePost_id>\d+)/CodeArticle.html', views.CodeArticle, name='CodePostDetails'),
+	url(r'^blog/(?P<DesignPost_id>\d+)/DesignArticle.html', views.DesignArticle, name='DesignPostDetails'),
 ]
