90c68da98c16b69dbe85bd10429a0943b7b24508
Nicole <nicole.rego@live.com>
2015-04-22 17:24:36 -0400

    NA-007 Editing view to define variables that store single article, setting up templates for CodePosts and DesignPosts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import ArticleInfo
+from .models import CodePost
+from .models import DesignPost
 
 def article_list(request):
+	'''
+	Querying the db to get articles that have been published,
+	ordering them by published most recently and storing it 
+	in each variable.
+	'''
+	CodeArticles = CodePost.objects.filter(published_on__lte=timezone.now()).order_by('published_on')
+	DesignArticles = DesignPost.objects.filter(published_on__lte=timezone.now()).order_by('published_on')
+
 	'''
 	Takes request and returns method render
 	to put together article_list template
 	'''
-	
-	return render(request, 'blog/article_list.html', {})
+	return render(request, 'blog/article_list.html', {'CodeArticles': CodeArticles, 'DesignArticles': DesignArticles })
+
+def CodeArticle(request, CodePost_id):
+	'''
+	Defines a variable to store a single article.
+	This is used in the template for CodePost details.
+	'''
+	# try:
+	CodeArticle = CodePost.objects.get(pk= CodePost_id)
+	# except CodePost.DoesNotExist:
+		# raise Http404
+	return render(request, 'blog/CodeArticle.html', {'CodeArticle': CodeArticle})
+
+def DesignArticle(request, DesignPost_id):
+	'''
+	Defines a variable to store a single article. 
+	This is used in the template for the DesignPost details.
+	'''
+	DesignArticle = DesignPost.objects.get(pk= DesignPost_id)
+	return render(request, 'blog/DesignArticle.html', {'DesignArticle': DesignArticle})
