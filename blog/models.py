17b8e95446d9eb92aa401f661c52145f9fa10367
Nicole <nicole.rego@live.com>
2015-04-22 10:13:30 -0400

    NA-002 Creating Article model and migrating

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Article(models.Model):
+	'''
+	Defining the Article model
+	'''
+	title = models.CharField(max_length = 200)
+	text = models.TextField()
+	author = models.ForeignKey('auth.User')
+	created_on = models.DateTimeField(default=timezone.now)
+	published_on = models.DateTimeField(blank=True, null=True)
+
+	def publish(self):
+		'''
+		Set the publishing time of an article 
+		and persist it to the database
+		'''
+		self.published_on = timezone.now()
+		self.save()
+
+	def __str__(self):
+		'''
+		Return a string with the Artcile title
+		'''
+		return self.title
